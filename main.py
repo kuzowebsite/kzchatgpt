ecdd38bd2795eaaa07d0e1f9f6e8df0a0269d1a2
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2025-01-08 03:08:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ openai.api_key = "sk-proj-xCilmld0qrMVwGYaexoB0N4EmvUjCHh_AdWZPyRFuOuguhz0gmkZHd
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
